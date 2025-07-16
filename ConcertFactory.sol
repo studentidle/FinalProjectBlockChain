a89b7fc2a32a929cbc24c30319fdab2fd86e21bf
studentidle <jedale.sabio@student.ateneo.edu>
2025-07-16 18:20:08 +0800

    added ConcertFactory.sol

---
+++ ConcertFactory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+contract ConcertFactory {
+    // This contract is a placeholder for the ConcertFactory
+    // It can be extended with functions to manage concerts, artists, and tickets
+    // For now, it serves as a base for future development
+}
