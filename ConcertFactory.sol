847854f1a883a99a8cfa039573f601fd9ed72fb2
studentidle <jedale.sabio@student.ateneo.edu>
2025-07-16 19:51:41 +0800

    added ConcertContract

--- ConcertFactory.sol
+++ ConcertFactory.sol
@@ -3,6 +3,12 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract ConcertFactory {
     // This contract is a placeholder for the ConcertFactory
-    // It can be extended with functions to manage concerts, artists, and tickets
+    // It can be extended with functions to generate concerts and tickets
+    // For now, it serves as a base for future development
+}
+
+contract ConcertContract {
+    // This contract is a placeholder for the ConcertContract
+    // It can be extended with functions to manage concert details, ticket sales, and attendance
     // For now, it serves as a base for future development
 }
