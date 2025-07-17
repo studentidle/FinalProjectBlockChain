652e3789c038ea1ef37cb7febb611e016aa55bfd
studentidle <jedale.sabio@student.ateneo.edu>
2025-07-17 21:32:38 +0800

    fixed bug in assiging ticketId in buyTicket

--- ConcertFactory.sol
+++ ConcertFactory.sol
@@ -70,7 +70,7 @@ contract ConcertContract {
         require(msg.value == ticketPrice, "Incorrect ticket price.");
         require(totalTicket > 0, "No tickets available.");
         require(msg.sender != organizer, "Organizer cannot buy tickets.");
-        require(ticketOwner[ticketId] == address(0), "Ticket already owned.");
+        require(ticketOwner[ticketId++] == address(0), "Ticket already owned.");
         //require(ticketsOwned[msg.sender] < 1, "You can only purchase one ticket at a time.");
 
         //ADDED: Create customer record
@@ -93,6 +93,21 @@ contract ConcertContract {
         //ADDED: Decrease total tickets available
         totalTicket--;
     }
+
+    function getMyTickets() public view returns (uint[] memory) {
+        return buyers[msg.sender].ticketIds; // Return the list of ticket IDs owned by the buyer
+    }
+
+    function returnCurrentTicketId() public view returns (uint) {
+        return ticketId; // Return the current ticket ID
+    }
+
+
+
+
+
+
+    
 
 
 
